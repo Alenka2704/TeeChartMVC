625fe56385b74004b425dcfdb75566eaff49dbd0
osavko <osavko@egc-cb.cz>
2019-05-03 15:15:52 +0200

    timeline graph "Invalid date"

---
+++ TeeChartWeb1/Scripts/graphs/timelineCustomCode.js
@@ -0,0 +1,38 @@
+﻿$(JSLINQ(chart1.axes.items).Where(function (item1) { return item1.horizontal; }).items).each(function () {
+	this.labels.dateFormat = "dd'.'mm'.'yyyy\nHH:MM";
+	this.labels.wordWrap = "yes";
+});
+
+var axesOn = false;
+$('#axes-btn').click(function () {
+	axesOn = !axesOn;
+	if (axesOn) {
+		chart1.axes.items[3].visible = false;
+		$(JSLINQ(chart1.axes.items).Where(function (item1) { return item1.horizontal && item1.title.text.indexOf("_") > -1; }).items).each(function () {
+			this.visible = true;
+		});
+		$(JSLINQ(chart1.series.items).items).each(function () {
+			var that = this;
+			that.horizAxis = $(JSLINQ(chart1.axes.items).Where(function (item1) {
+				return item1.horizontal && that.title.indexOf("_") > -1 && that.title.substring(that.title.indexOf("_") + 1) == item1.title.text.substring(item1.title.text.indexOf("_") + 1);
+			}))[0].items[0];
+		});
+	}
+	else {
+		$(JSLINQ(chart1.axes.items).Where(function (item1) { return item1.horizontal; }).items).each(function () {
+			this.visible = false;
+		});
+		chart1.axes.items[3].visible = true;
+		$(JSLINQ(chart1.series.items).items).each(function () {
+			this.horizAxis = "bottom";
+		});
+	}
+	chart1.draw();
+});
+
+$(document).ready(() => {
+	var body = $(document.body)[0];
+	var canvas = body.getElementsByTagName("canvas")[1];
+	canvas.remove();
+	chart1.draw();
+});
---
+++ TeeChartWeb1/Scripts/graphs/timelineExternalCode.js
@@ -0,0 +1,13 @@
+﻿function resize(chart) {
+	if (chart != null) {
+		var div = $('#timelineCanvasDiv');
+		var w = div.width();
+		var h = div.height();
+		var canvas = chart.canvas;
+		canvas.setAttribute('width', "" + w + "px");
+		canvas.setAttribute('height', "" + h + "px");
+		chart.bounds.width = w;
+		chart.bounds.height = h;
+		chart.draw();
+	}
+}
